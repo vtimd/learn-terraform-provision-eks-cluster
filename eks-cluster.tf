c9db3111b70618784822dab946492309ea9296a3
Tim Davis <tim.davis.instinct@gmail.com>
2020-12-01 13:23:59 -0600

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -25,7 +25,7 @@ module "eks" {
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 2
     },
   ]
 }
